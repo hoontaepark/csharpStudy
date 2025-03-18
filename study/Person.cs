2547342ce403dbabb663098c00eb71147e10b858
hoontaepark <pthomj13@gmail.com>
2025-03-18 16:04:54 +0900

    class 생성

---
+++ study/Person.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Person{
+    public string name;
+    public string birthday;
+    public string Gender;
+
+    public void Eat(){
+        Console.WriteLine(name + " is eating");
+    }
+
+    public void Walk(){
+        Console.WriteLine(name + " is walking");
+    }
+
+    public void Run(){
+        Console.WriteLine(name + " is running");
+    }
+    
+}
