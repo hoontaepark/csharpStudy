6840afd694ea711524d1b88c775efb4af2115806
hoontaepark <pthomj13@gmail.com>
2025-03-19 13:42:34 +0900

    queue

--- obj/Debug/net9.0/csharp_study_01.AssemblyInfo.cs
+++ obj/Debug/net9.0/csharp_study_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_study_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0834c6586b18c5dc5242952c5a2a619c86e06ae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164e276b92fdfc458f16cf66bc035876b8ff58a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_study_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_study_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
